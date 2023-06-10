8ef57a7016e021cb7dcdff7cc05efeeb28d0224c
shamimislam <134716211+shamim-devoloper@users.noreply.github.com>
2023-06-10 21:36:09 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var myVar =    document.querySelector("#paraId");
+function addStyle()
+{
+myVar.classList.add("para-style");
+
+}
+
+
+function removeStyle()
+{
+myVar.classList.remove("para-style");
+
+}
